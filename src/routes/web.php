084100a4216ef8003bded86f6b4c206307863450
yui hito <105.love.children@gmail.com>
2026-05-23 13:37:24 +0900

    認証ルート整理とauthミドルウェア適用

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeightLogController;
 use App\Http\Controllers\WeightTargetController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AuthController;
 
 
 
@@ -18,18 +19,19 @@ use App\Http\Controllers\RegisterController;
 |
 */
 
-Route::get('/login', [RegisterController::class, 'index'])->name('login');
-Route::get('/register/step1', [RegisterController::class, 'step1'])->name('register_step1');
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/register/step1', [AuthController::class, 'step1'])->name('register_step1');
 Route::get('/register/step2', [WeightTargetController::class, 'step2'])->name('register_step2');
 Route::post('/register/step2', [WeightTargetController::class, 'storeStep2'])->name('register_step2_store');
 
-Route::get('/weight_logs', [WeightLogController::class, 'index'])->name('weight_logs.index');
-Route::post('/weight_logs', [WeightLogController::class, 'storeModal'])->name('weight_logs.store');//モーダルからの登録処理ルート
-Route::get('/weight_logs/create', [WeightLogController::class, 'create'])->name('weight_logs.create'); //モーダル表示ルート
-Route::get('/weight_logs/search', [WeightLogController::class, 'search'])->name('weight_logs.search');
-Route::get('/weight_logs/goal_setting', [WeightTargetController::class, 'goalSetting'])->name('weight_logs.goal_setting');
-Route::post('/weight_logs/goal_setting', [WeightTargetController::class, 'updateGoal'])->name('weight_logs.update_goal');
-Route::get('/weight_logs/{weightLogId}', [WeightLogController::class, 'detail'])->name('weight_logs.detail');
-Route::patch('/weight_logs/{weightLogId}/update', [WeightLogController::class, 'update'])->name('weight_logs.update');
-Route::get('/weight_logs/{weightLogId}/delete', [WeightLogController::class, 'delete'])->name('weight_logs.delete');
-Route::get('/logout', [RegisterController::class, 'logout'])->name('weight_logs.logout');
+Route::middleware('auth')->group(function () {
+    Route::get('/weight_logs', [WeightLogController::class, 'index'])->name('weight_logs.index');
+    Route::post('/weight_logs', [WeightLogController::class, 'storeModal'])->name('weight_logs.store'); //モーダルからの登録処理ルート
+    Route::get('/weight_logs/create', [WeightLogController::class, 'create'])->name('weight_logs.create'); //モーダル表示ルート
+    Route::get('/weight_logs/search', [WeightLogController::class, 'search'])->name('weight_logs.search');
+    Route::get('/weight_logs/goal_setting', [WeightTargetController::class, 'goalSetting'])->name('weight_logs.goal_setting');
+    Route::post('/weight_logs/goal_setting', [WeightTargetController::class, 'updateGoal'])->name('weight_logs.update_goal');
+    Route::get('/weight_logs/{weightLogId}', [WeightLogController::class, 'detail'])->name('weight_logs.detail');
+    Route::patch('/weight_logs/{weightLogId}/update', [WeightLogController::class, 'update'])->name('weight_logs.update');
+    Route::get('/weight_logs/{weightLogId}/delete', [WeightLogController::class, 'delete'])->name('weight_logs.delete');
+});
